f9c14c4ea080198339cb8f847829cb8daab4e031
Alweezy <alvinmutisya@gmail.com>
2017-08-02 10:28:56 +0300

    [Fix #149551607]Restore settings

--- manage.py
+++ manage.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
-import os
 import sys
 
 from django.core.management import execute_from_command_line
 
-# from tasks import (
-#     setup_django_environment,
-#     get_user_config_path
-# )
+from tasks import (
+    setup_django_environment,
+    get_user_config_path
+)
 
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
-    # if not any('--settings' in s for s in sys.argv):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    if not any('--settings' in s for s in sys.argv):
+        setup_django_environment(get_user_config_path('wger', 'settings.py'))
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
